8cc3ab3ded220cff9aafcb0656134a4e5e2d6e31
Aquib <aquibu1807@gmail.com>
2024-04-09 11:23:53 +0530

    added middleware and function to handle error

--- api/index.js
+++ api/index.js
@@ -18,9 +18,19 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
