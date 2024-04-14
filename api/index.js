8b415e99273484e023d9bdec61fbedd13c5df4f9
Aquib <aquibu1807@gmail.com>
2024-04-14 14:56:09 +0530

    complete update functionality api and ui

--- api/index.js
+++ api/index.js
@@ -3,28 +3,31 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log('Connected to database');
+    console.log('Connected to MongoDB!');
   })
   .catch((err) => {
     console.log(err);
   });
 
 const app = express();
-const PORT = 3000;
 
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
 });
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
