901804ac535b22c1e29a37028b6726dc9a34bc30
Aquib <aquibu1807@gmail.com>
2024-06-27 21:38:36 +0530

    Complete Upload Listing Image Functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,10 +1,84 @@
-import React from 'react';
+import { useState } from 'react';
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from '../firebase';
+import { set } from 'mongoose';
 
 export default function CreateListing() {
+  const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+  });
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [uploading, setUploading] = useState(false);
+  console.log(formData);
+  const handleImageSubmit = (e) => {
+    if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+      setUploading(true);
+      setImageUploadError(false);
+      const promises = [];
+
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+          setImageUploadError(false);
+          setUploading(false);
+        })
+        .catch((err) => {
+          setImageUploadError('Image upload failed (2 mb max per image)');
+          setUploading(false);
+        });
+    } else {
+      setImageUploadError('You can only upload 6 images per listing');
+      setUploading(false);
+    }
+  };
+
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload is ${progress}% done`);
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+
+  const handleRemoveImage = (index) => {
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, i) => i !== index),
+    });
+  };
   return (
     <main className='p-3 max-w-4xl mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>
-        Create Listing
+        Create a Listing
       </h1>
       <form className='flex flex-col sm:flex-row gap-4'>
         <div className='flex flex-col gap-4 flex-1'>
@@ -16,21 +90,21 @@ export default function CreateListing() {
             maxLength='62'
             minLength='10'
             required
-          ></input>
+          />
           <textarea
             type='text'
             placeholder='Description'
             className='border p-3 rounded-lg'
             id='description'
             required
-          ></textarea>
+          />
           <input
             type='text'
             placeholder='Address'
             className='border p-3 rounded-lg'
             id='address'
             required
-          ></input>
+          />
           <div className='flex gap-6 flex-wrap'>
             <div className='flex gap-2'>
               <input type='checkbox' id='sale' className='w-5' />
@@ -42,7 +116,7 @@ export default function CreateListing() {
             </div>
             <div className='flex gap-2'>
               <input type='checkbox' id='parking' className='w-5' />
-              <span>Parking Spot</span>
+              <span>Parking spot</span>
             </div>
             <div className='flex gap-2'>
               <input type='checkbox' id='furnished' className='w-5' />
@@ -61,7 +135,7 @@ export default function CreateListing() {
                 min='1'
                 max='10'
                 required
-                className='p-3 border-gray-300 rounded-lg'
+                className='p-3 border border-gray-300 rounded-lg'
               />
               <p>Beds</p>
             </div>
@@ -72,7 +146,7 @@ export default function CreateListing() {
                 min='1'
                 max='10'
                 required
-                className='p-3 border-gray-300 rounded-lg'
+                className='p-3 border border-gray-300 rounded-lg'
               />
               <p>Baths</p>
             </div>
@@ -83,11 +157,11 @@ export default function CreateListing() {
                 min='1'
                 max='10'
                 required
-                className='p-3 border-gray-300 rounded-lg'
+                className='p-3 border border-gray-300 rounded-lg'
               />
               <div className='flex flex-col items-center'>
-                <p>Regular Price</p>
-                <span className='text-xs'>(₹ / month)</span>
+                <p>Regular price</p>
+                <span className='text-xs'>($ / month)</span>
               </div>
             </div>
             <div className='flex items-center gap-2'>
@@ -97,11 +171,11 @@ export default function CreateListing() {
                 min='1'
                 max='10'
                 required
-                className='p-3 border-gray-300 rounded-lg'
+                className='p-3 border border-gray-300 rounded-lg'
               />
               <div className='flex flex-col items-center'>
-                <p>Discounted Price</p>
-                <span className='text-xs'>(₹ / month)</span>
+                <p>Discounted price</p>
+                <span className='text-xs'>($ / month)</span>
               </div>
             </div>
           </div>
@@ -110,21 +184,51 @@ export default function CreateListing() {
           <p className='font-semibold'>
             Images:
             <span className='font-normal text-gray-600 ml-2'>
-              The first image will be a cover (max 6)
+              The first image will be the cover (max 6)
             </span>
           </p>
+
           <div className='flex gap-4'>
             <input
+              onChange={(e) => setFiles(e.target.files)}
               className='p-3 border border-gray-300 rounded w-full'
               type='file'
               id='images'
               accept='image/*'
               multiple
             />
-            <button className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>
-              Upload
+            <button
+              type='button'
+              disabled={uploading}
+              onClick={handleImageSubmit}
+              className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'
+            >
+              {uploading ? 'Uploading...' : 'Upload'}
             </button>
           </div>
+          <p className='text-red-700 text-sm'>
+            {imageUploadError && imageUploadError}
+          </p>
+          {formData.imageUrls.length > 0 &&
+            formData.imageUrls.map((url, index) => (
+              <div
+                key={url}
+                className='flex justify-between p-3 border items-center'
+              >
+                <img
+                  src={url}
+                  alt='listing image'
+                  className='w-20 h-20 object-contain rounded-lg'
+                />
+                <button
+                  type='button'
+                  onClick={() => handleRemoveImage(index)}
+                  className='p-3 text-red-700 rounded-lg uppercase hover:opacity-75'
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
           <button className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
             Create Listing
           </button>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -180,7 +180,9 @@ export default function Profile() {
         <Link
           className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
           to={'/create-listing'}
-        ></Link>
+        >
+          Create Listing
+        </Link>
       </form>
       <div className='flex justify-between mt-5'>
         <span onClick={handleDelete} className='text-red-700 cursor-pointer'>
