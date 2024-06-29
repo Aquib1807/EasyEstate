060b7a87d8161d811d98730e558879541ad8bc93
Aquib <aquibu1807@gmail.com>
2024-06-29 13:31:11 +0530

    added about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,37 @@
 import React from 'react';
+import Team from '../components/Team';
 
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        About Sahand Estate
+      </h1>
+      <p className='mb-4 text-slate-700'>
+        Finding your dream home shouldn't feel like a chore. Introducing
+        EasyEstate, the innovative real estate platform developed by Mohd Aquib
+        Usmani. We take the stress out of renting, buying, and selling by
+        offering a user-friendly experience and a comprehensive suite of tools.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        At EasyEstate, you'll discover a vast selection of properties, from cozy
+        studios to luxurious estates. Our powerful search engine allows you to
+        pinpoint exactly what you're looking for, filtering by location, price
+        range, size, and even specific amenities. Whether you're a renter
+        seeking the perfect place to call home, a buyer embarking on a thrilling
+        new chapter, or a seller ready to capitalize on the market, Easyestate
+        empowers you to navigate the process with confidence.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        At Easyestate, you'll discover a vast selection of properties, from cozy
+        studios to luxurious estates. Our powerful search engine allows you to
+        pinpoint exactly what you're looking for, filtering by location, price
+        range, size, and even specific amenities. Whether you're a renter
+        seeking the perfect place to call home, a buyer embarking on a thrilling
+        new chapter, or a seller ready to capitalize on the market, Easyestate
+        empowers you to navigate the process with confidence.
+      </p>
+      <Team />
+    </div>
+  );
 }
